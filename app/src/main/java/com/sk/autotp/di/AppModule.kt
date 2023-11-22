6e0047778c87a16f054b1cc7a4f15713bd7e0145
Swapnil Kadlag <swapnil.kadlag.27@gmail.com>
2023-11-22 21:40:04 +0530

    Add DI for application wide coroutine scope

--- app/src/main/java/com/sk/autotp/di/AppModule.kt
+++ app/src/main/java/com/sk/autotp/di/AppModule.kt
@@ -8,11 +8,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
     @Provides
     fun provideDb(@ApplicationContext application: Context): AutOTPDatabase {
         return Room.databaseBuilder(
@@ -20,5 +25,11 @@ object AppModule {
             AutOTPDatabase::class.java,
             "AutOTP_db",
         ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesCoroutineScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.IO)
     }
 }
