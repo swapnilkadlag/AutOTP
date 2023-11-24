6292996c839c5f68c8a438643fae4959764d0364
Swapnil Kadlag <swapnil.kadlag.27@gmail.com>
2023-11-24 21:03:06 +0530

    Add Repository

---
+++ app/src/main/java/com/sk/autotp/data/Repository.kt
@@ -0,0 +1,40 @@
+package com.sk.autotp.data
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.util.UUID
+import javax.inject.Inject
+
+class Repository @Inject constructor(
+    private val db: AutOTPDatabase,
+    private val scope: CoroutineScope,
+) {
+
+    fun insertRule(rule: Rule) {
+        scope.launch { db.ruleDao.insert(rule) }
+    }
+
+    fun updateRule(rule: Rule) {
+        scope.launch { db.ruleDao.update(rule) }
+    }
+
+    fun deleteRule(rule: Rule) {
+        scope.launch { db.ruleDao.delete(rule) }
+    }
+
+    suspend fun getRule(ruleId: UUID): Rule = db.ruleDao.get(ruleId)
+
+    fun getRules() = db.ruleDao.getAll()
+
+    suspend fun getEnabledRules(): List<Rule> = db.ruleDao.getAllEnabled()
+
+    fun updateRuleStatus(ruleId: UUID, enabled: Boolean) {
+        scope.launch { db.ruleDao.updateStatus(ruleId, enabled) }
+    }
+
+    fun insertLog(log: Log) {
+        scope.launch { db.logDao.insert(log) }
+    }
+
+    fun getLogs() = db.logDao.getAll()
+}
